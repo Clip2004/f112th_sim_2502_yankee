fe7223c5c6cf9ffee980d911c0c686c1c697144b
Clip2004 <felipe.mercado59@eia.edu.co>
2025-07-31 17:37:21 -0500

    Se cambio la posición en la que aparece el carro en el launch

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -31,10 +31,18 @@ def generate_launch_description():
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
-                        output='screen')
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-topic', 'robot_description',
+            '-entity', 'my_bot',
+            '-x', '2.0',
+            '-y', '-6.0',
+            '-z', '0.05',      # altura desde el suelo (usualmente 0.0 o 0.1)
+        ],
+        output='screen'
+    )   
 
     joystick = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
